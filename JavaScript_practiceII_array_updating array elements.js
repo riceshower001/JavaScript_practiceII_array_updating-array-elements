618c9cd4ee1916e6d7fd80b9c2e9e077f15449f7
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-27 02:48:48 +0900

    Add files via upload

---
+++ JavaScript_practiceII_array_updating array elements.js
@@ -0,0 +1,9 @@
+// Let's update the elements of an array.
+
+const animals = ["dog", "cat", "sheep"];
+
+// Please update the third element of an array "animals" to "rabbit".
+animals[2] = "rabbit" ;
+
+// Please print the third element of an array "animals".
+console.log(animals[2]);
